5f7fe0a5ebb5aa81d15c1783e51b7b56e6b523ce
嘉旭 <jiaxue.ljl@alibaba-inc.com>
2015-09-24 19:33:31 +0800

    Carthage project

---
+++ SwiftRouter/SwiftRouter.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRouter.h
+//  SwiftRouter
+//
+//  Created by skyline on 15/9/24.
+//  Copyright © 2015年 skyline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftRouter.
+FOUNDATION_EXPORT double SwiftRouterVersionNumber;
+
+//! Project version string for SwiftRouter.
+FOUNDATION_EXPORT const unsigned char SwiftRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRouter/PublicHeader.h>
+
+
